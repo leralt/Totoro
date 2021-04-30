cea1f297bb322b5b36dd05edb58652e1b960d8cd
long <htliucl@foxmail.com>
2021-04-30 17:18:23 +0800

    init project

---
+++ cmake-build-debug/CMakeFiles/Totoro.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Totoro.dir/main.cpp.o"
+  "Totoro"
+  "Totoro.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Totoro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Totoro.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Totoro.
+# This may be replaced when dependencies are built.
